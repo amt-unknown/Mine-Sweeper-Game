097adab4b3d06a917d4579768d60f3c23baea296
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-02-23 19:38:31 -0800

    Basic grid idea for DOM handling

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello World")
+//Game grid sytem
+//Create DOM for Grid
+
+//Generate Mine Map
+//Fixed Grid System(for initial install)
+
+//*Randomized Grid System
+
+//Generate Numbers Map
+
+//Game grid sensor. Checks targeted block for underlying type
+
+
+
+
+
+
+
+//Score Tracker
+
+//*Game difficulty selection
+
+
+
+
+//Local Storage and Leaderboard
+
+
+
+
+
+//*Tutorial
+//*Help Button
